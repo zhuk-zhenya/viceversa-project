bfacd04363d70346d97e43b7b76b4a73ebdd03c7
zhuk-zhenya <88724582+zhuk-zhenya@users.noreply.github.com>
2022-02-09 11:03:55 +0300

    add reverse.html

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('reverse/', views.reverse, name='reverse'),
+
 ]
--- viceversa/views.py
+++ viceversa/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home.html')
 	
+
+def reverse(request):
+	return render(request, 'reverse.html')
+	
