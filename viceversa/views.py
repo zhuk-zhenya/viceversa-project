802017b171994bf548dbff19c543978280dfeb9d
zhuk-zhenya <88724582+zhuk-zhenya@users.noreply.github.com>
2022-02-09 11:20:46 +0300

    add reverse feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,5 +6,7 @@ def home(request):
 	
 
 def reverse(request):
-	return render(request, 'reverse.html')
+	user_text = request.GET['usertext']
+	reverse_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext':user_text,'reverse_text':reverse_text})
 	
